70c843b966011d991f020eab5b7d4fe2e945df9f
JennyPereira <jfpereira1@utpl.edu.ec>
2019-07-13 11:35:30 -0500

    trabajando en movimientos

--- js/start.js
+++ js/start.js
@@ -18,13 +18,35 @@ function start(){
 }
 
 function bs(){
-    $('#mov_1').show(); 
-    var conte = document.getElementById('mov_1');
-    $('#mov_1').addClass('uno');
+    $('#paq_1').show(); 
+    var conte = document.getElementById('paq_1');
+    var tamanioVentana = jQuery('#valorTamanioVentana').text();
+    $('#paq_1').addClass('uno');
     conte.addEventListener("animationend", function(){
         //funcion para comparar.. 
-        $('#mov_1').removeClass('uno');  
-        $('#mov_1').css("display", "none");    
+        $('#paq_1').removeClass('uno');  
+        $('#paq_1').css("display", "none");
+        if(tamanioVentana==1){
+
+        }else if(tamanioVentana==2){
+            ventana_de_2();
+        }else if(tamanioVentana==3){
+            ventana_de_3;
+        }else if(tamanioVentana==4){
+            ventana_de_4;
+        }
+        //alert(tamanioVentana);
     }, false);
+}
+
+function ventana_de_2{
+
+}
+
+function ventana_de_3{
     
+}
+
+function ventana_de_4{
+
 }
